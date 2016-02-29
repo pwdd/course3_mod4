b246db2ec7e37278580b8062c2f7dbead9357fe1
pwdd <ainapinto@gmail.com>
2016-02-28 19:28:54 -0800

    custom type rq02

---
+++ app/models/address.rb
@@ -0,0 +1,34 @@
+class Address
+  attr_accessor :city, :state, :location
+
+  def initialize(city, state, location)
+    @city = city
+    @state = state
+    @location = location
+  end
+
+  def mongoize
+    { city: city, state: state, loc: location.mongoize }
+  end
+
+  def self.mongoize(address)
+    return nil if address.nil?
+    if address.is_a?(Address)
+      loc = address.location.mongoize
+      { city: address.city, state: address.state, loc: loc }
+    else
+      address
+    end
+  end
+
+  def self.demongoize(address)
+    return nil if address.nil?
+    loc_array = address[:loc][:coordinates]
+    loc = Point.new(*loc_array)
+    Address.new(address[:city], address[:state], loc)
+  end
+
+  def self.evolve(address)
+    Address.mongoize(address)
+  end
+end
