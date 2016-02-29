40c5673abc9d4b9ca460fa27a3398829e17b482f
pwdd <ainapinto@gmail.com>
2016-02-29 00:18:29 -0800

    racers spec rq03

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -1,10 +1,12 @@
 class RacerInfo
   include Mongoid::Document
-  field :first_name, as: :fn, type: String
-  field :last_name, as: :ln, type: String
-  field :gender, as: :g, type: String
-  field :birth_year, as: :yr, type: Integer
-  field :residence, as: :res, type: Address
+  field :fn, as: :first_name, type: String
+  field :ln, as: :last_name, type: String
+  field :g, as: :gender, type: String
+  field :yr, as: :birth_year, type: Integer
+  field :res, as: :residence, type: Address
   field :racer_id, as: :_id
   field :_id, default: ->{ racer_id }
+
+  embedded_in :parent, polymorphic: true
 end
