d385c1d63bf50aa1d61f38d44b7f66c80a511918
pwdd <ainapinto@gmail.com>
2016-02-29 23:37:08 -0800

    racer results rq01

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -12,6 +12,7 @@ class Entrant
 
   embeds_many :results, class_name: 'LegResult', order: [:'event.o'.asc], after_add: :update_total
   embeds_one :race, class_name: 'RaceRef'
+  embeds_one :racer, as: :parent, class_name: 'RacerInfo'
 
   def update_total(result)
     self.secs = 0
