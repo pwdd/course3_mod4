56e75e6d15379c4d30f3dd7785d1bf2f41f0ec5c
pwdd <ainapinto@gmail.com>
2016-03-01 13:44:01 -0800

    service facade rq01

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,6 +4,7 @@ class Race
   field :n, as: :name, type: String
   field :date, type: Date
   field :loc, as: :location, type: Address
+  field :next_bib, type: Integer, default: 0
 
   embeds_many :events, as: :parent, order: [:order.asc]
   has_many :entrants, 
@@ -65,4 +66,8 @@ class Race
     end
   end
 
+  def next_bib 
+    self.inc(next_bib: 1)
+    self[:next_bib]
+  end
 end
